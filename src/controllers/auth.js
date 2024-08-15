cd42bd7c4ce4fab9cf2658ab9de6cd57d9d5c3da
vika59216 <viktoriiadupreez@gmail.com>
2024-08-15 22:57:10 +0200

    hw5

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -7,7 +7,7 @@ import { refreshUsersSession } from '../services/auth.js';
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
 
-  res.json({
+  res.status(201).json({
     status: 201,
     message: 'Successfully registered a user!',
     data: user,
