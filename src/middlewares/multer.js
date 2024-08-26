be4241a120a717ffb197f70180660d2c749b73d9
vika59216 <viktoriiadupreez@gmail.com>
2024-08-26 20:31:32 +0200

    hw6

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,6 +1,7 @@
 import multer from 'multer';
 import { TEMP_UPLOAD_DIR } from '../constants/index.js';
 
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, TEMP_UPLOAD_DIR);
